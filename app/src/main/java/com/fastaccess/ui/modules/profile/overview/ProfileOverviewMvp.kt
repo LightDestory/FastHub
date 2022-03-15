a71897254e4b9f7ae310d5e565de73f81168d827
LightDestory <apb231@gmail.com>
2022-03-16 00:55:39 +0100

    feat: add Profile README on Profile Overview

--- app/src/main/java/com/fastaccess/ui/modules/profile/overview/ProfileOverviewMvp.kt
+++ app/src/main/java/com/fastaccess/ui/modules/profile/overview/ProfileOverviewMvp.kt
@@ -1,12 +1,12 @@
 package com.fastaccess.ui.modules.profile.overview
 
-import com.fastaccess.ui.base.mvp.BaseMvp.FAView
-import com.fastaccess.ui.base.mvp.BaseMvp.FAPresenter
 import android.os.Bundle
 import com.fastaccess.data.dao.model.User
 import com.fastaccess.github.GetPinnedReposQuery
-import com.fastaccess.ui.widgets.contributions.GitHubContributionsView
+import com.fastaccess.ui.base.mvp.BaseMvp.FAPresenter
+import com.fastaccess.ui.base.mvp.BaseMvp.FAView
 import com.fastaccess.ui.widgets.contributions.ContributionsDay
+import com.fastaccess.ui.widgets.contributions.GitHubContributionsView
 
 /**
  * Created by Kosh on 03 Dec 2016, 9:15 AM
@@ -19,6 +19,7 @@ interface ProfileOverviewMvp {
         fun onInitOrgs(orgs: List<User>)
         fun onUserNotFound()
         fun onInitPinnedRepos(nodes: List<GetPinnedReposQuery.Node>)
+        fun onSetMdText(text: String, baseUrl: String, replace: Boolean)
     }
 
     interface Presenter : FAPresenter {
@@ -34,5 +35,6 @@ interface ProfileOverviewMvp {
         val contributions: MutableList<ContributionsDay>
         val nodes: List<GetPinnedReposQuery.Node>
         val login: String?
+        var readmeFetch: Boolean
     }
 }
