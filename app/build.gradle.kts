d9c62861f2da00e24d79293252befe4366d3ab0c
LightDestory <apb231@gmail.com>
2022-03-17 15:28:46 +0100

    feat: release build system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,25 @@ plugins {
     id("com.mikepenz.aboutlibraries.plugin").version("10.0.0-rc02")
 }
 
+fun loadConfig(): HashMap<String, String> {
+    val configs: HashMap<String, String> = HashMap()
+    configs["GITHUB_CLIENT_ID"] = "473e333123519beadd63"
+    configs["GITHUB_SECRET"] = "b2d158f949d3615078eaf570ff99eba81cfa1ff9"
+    configs["IMGUR_CLIENT_ID"] = "5fced7f255e1dc9"
+    configs["IMGUR_SECRET"] = "03025033403196a4b68b48f0738e67ef136ad64f"
+    val inputFile = rootProject.file("${rootProject.projectDir}\\app\\secrets.properties")
+    (inputFile.exists()).let {
+        println("Loading release configs")
+        inputFile.forEachLine {
+            val data = it.split("=")
+                configs[data[0]] = data[1]
+        }
+    }
+    return configs
+}
+
+val config = loadConfig()
+
 android {
     namespace = "com.fastaccess"
     compileSdk = 31
@@ -17,10 +36,10 @@ android {
         versionCode = 473
         versionName = "4.7.3"
         buildConfigField("String", "GITHUB_APP_ID", "\"com.fastaccess.github.revival\"")
-        buildConfigField("String", "GITHUB_CLIENT_ID", "\"473e333123519beadd63\"")
-        buildConfigField("String", "GITHUB_SECRET", "\"b2d158f949d3615078eaf570ff99eba81cfa1ff9\"")
-        buildConfigField("String", "IMGUR_CLIENT_ID", "\"5fced7f255e1dc9\"")
-        buildConfigField("String", "IMGUR_SECRET", "\"03025033403196a4b68b48f0738e67ef136ad64f\"")
+        buildConfigField("String", "GITHUB_CLIENT_ID", "\"${config["GITHUB_CLIENT_ID"]}\"")
+        buildConfigField("String", "GITHUB_SECRET", "\"${config["GITHUB_SECRET"]}\"")
+        buildConfigField("String", "IMGUR_CLIENT_ID", "\"${config["IMGUR_CLIENT_ID"]}\"")
+        buildConfigField("String", "IMGUR_SECRET", "\"${config["IMGUR_SECRET"]}\"")
         buildConfigField("String", "REST_URL", "\"https://api.github.com/\"")
         buildConfigField("String", "IMGUR_URL", "\"https://api.imgur.com/3/\"")
         buildConfigField("String", "GITHUB_STATUS_URL", "\"https://www.githubstatus.com/\"")
@@ -30,7 +49,13 @@ android {
     }
     signingConfigs {
         getByName("debug") {
-            storeFile = file("${rootProject.projectDir}\\debug.keystore")
+            storeFile = file("${rootProject.projectDir}\\app\\keys_debug.jks")
+        }
+        create("release") {
+            storeFile = file("${rootProject.projectDir}\\app\\keys_release.jks")
+            storePassword = config["STORE_PASSWORD"]
+            keyPassword = config["KEY_PASSWORD"]
+            keyAlias = "key0"
         }
     }
     buildTypes {
@@ -38,6 +63,7 @@ android {
             isShrinkResources = true
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release")
         }
         debug {
             signingConfig = signingConfigs.getByName("debug")
@@ -161,7 +187,7 @@ dependencies {
     // HtmlSpanner
     implementation("com.github.NightWhistler:HtmlSpanner:0.4")
     // htmlcleaner
-    implementation("net.sourceforge.htmlcleaner:htmlcleaner:2.2")
+    implementation("net.sourceforge.htmlcleaner:htmlcleaner:2.26")
 
 
     // commonmark
